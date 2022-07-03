d96ed5694ee3e34ab3653221161714f833f61541
Brian Crisp <bcrisp084@yahoo.com>
2022-07-02 22:48:51 -0400

    Made a request to the server to return the movie information

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
+  var url = "http://www.omdbapi.com/?apikey=" + apiKey + "&s=";
+
   $("#search-btn").on("click", function () {
     var searchVal = $("#search-val").val().trim();
-    console.log(searchVal);
+    $.ajax({
+      url: url + searchVal,
+      method: "GET",
+    }).then(function (response) {
+      var res = response.Search;
+      for (let i = 0; i < res.length; i++) {
+        let element = res[i].Poster;
+        var img = $("<img>");
+        img.attr("src", element);
+        $(".images").append(img);
+      }
+    });
   });
 });
