aed95c5d3a54ff59e9c9dcec0b46b7ac27f56002
Brian Crisp <bcrisp084@yahoo.com>
2022-08-04 20:36:21 -0400

    no changes were made

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,21 @@
 $(document).ready(function () {
-  var url = "http://www.omdbapi.com/?apikey=" + apiKey + "&s=";
+  var apiKey = "5119c315";
+  var url = "http://www.omdbapi.com/?plot=full&apikey=" + apiKey + "&t=";
 
   $("#search-btn").on("click", function () {
     var searchVal = $("#search-val").val().trim();
+    console.log(searchVal);
     $.ajax({
       url: url + searchVal,
       method: "GET",
     }).then(function (response) {
-      var res = response.Search;
-      for (let i = 0; i < res.length; i++) {
-        let element = res[i].Poster;
-        var img = $("<img>");
-        img.attr("src", element);
-        $(".images").append(img);
-      }
+      console.log(response);
+      var res = response.Poster;
+      console.log(res);
+      var image = $("<img>");
+      image.attr("src", res);
+      image.attr("class", "poster");
+      $(".images").append(image);
     });
   });
 });
