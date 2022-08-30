8b6bb93563e7adeec04bc34793ccfcf50129037c
Brian Crisp <bcrisp084@yahoo.com>
2022-08-29 21:24:08 -0400

    added attributes to retrieve info from each card to return a detailed search if the user clicks on a specific movie

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,49 @@
 $(document).ready(function () {
   var apiKey = "5119c315";
-  var url = "http://www.omdbapi.com/?plot=full&apikey=" + apiKey + "&t=";
+  var searchVal;
+  var url =
+    "http://www.omdbapi.com/?&apikey=" + apiKey + "&page=1&type=movie&s=";
+  var plotUrl = "http://www.omdbapi.com/?&apikey=" + apiKey + "&plot=short&t=";
 
   $("#search-btn").on("click", function () {
-    var searchVal = $("#search-val").val().trim();
-    console.log(searchVal);
+    searchVal = $("#search-val").val().trim();
     $.ajax({
       url: url + searchVal,
       method: "GET",
     }).then(function (response) {
-      console.log(response);
-      var res = response.Poster;
-      console.log(res);
-      var image = $("<img>");
-      image.attr("src", res);
-      image.attr("class", "poster");
-      $(".images").append(image);
+      // console.log(response.Search);
+      // console.log(url);
+      // console.log(response);
+      for (let i = 0; i < response.Search.length; i++) {
+        var movieEach = response.Search[i];
+        var title = $("<h3>" + movieEach.Title + "</h3>");
+        var image = $("<img>");
+        image.attr("src", movieEach.Poster);
+        image.attr("class", "poster");
+        var year = $("<div>Release Year : " + movieEach.Year + "</div>");
+        var cardDiv = $("<div>").attr("class", "card");
+        cardDiv.attr("data-name", movieEach.Title);
+        cardDiv.append(title, image, year);
+        $(".container").append(cardDiv);
+      }
     });
   });
+
+  $(document).on("click", ".card", function () {
+    console.log($(this));
+    console.log($(this).attr("data-name"));
+    $.ajax({
+      url: url,
+      method: "GET",
+    }).then(function () {});
+  });
+
+  // $("#search-btn").on("click", function () {
+  //   $.ajax({
+  //     url: plotUrl + searchVal,
+  //     Method: "GET",
+  //   }).then(function (response) {
+  //     console.log("plot", response);
+  //   });
+  // });
 });
