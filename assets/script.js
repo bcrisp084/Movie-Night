781595ff9c2510f816942295c86a7b155a55afc3
Brian Crisp <bcrisp084@yahoo.com>
2022-08-30 21:19:50 -0400

    created functionality to add a button to each movie returned that will load the full details of that movie when clicked

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
   var searchVal;
   var url =
     "http://www.omdbapi.com/?&apikey=" + apiKey + "&page=1&type=movie&s=";
-  var plotUrl = "http://www.omdbapi.com/?&apikey=" + apiKey + "&plot=short&t=";
 
   $("#search-btn").on("click", function () {
     searchVal = $("#search-val").val().trim();
@@ -11,39 +10,37 @@ $(document).ready(function () {
       url: url + searchVal,
       method: "GET",
     }).then(function (response) {
-      // console.log(response.Search);
-      // console.log(url);
-      // console.log(response);
       for (let i = 0; i < response.Search.length; i++) {
         var movieEach = response.Search[i];
-        var title = $("<h3>" + movieEach.Title + "</h3>");
+        var title = $("<h5>" + movieEach.Title + "</h5>");
         var image = $("<img>");
         image.attr("src", movieEach.Poster);
         image.attr("class", "poster");
-        var year = $("<div>Release Year : " + movieEach.Year + "</div>");
+        const movieInfo = $("<button>Movie Details</button>");
+        movieInfo.addClass("movieDetails");
         var cardDiv = $("<div>").attr("class", "card");
         cardDiv.attr("data-name", movieEach.Title);
-        cardDiv.append(title, image, year);
+        cardDiv.append(image, title, movieInfo);
         $(".container").append(cardDiv);
       }
     });
   });
 
-  $(document).on("click", ".card", function () {
-    console.log($(this));
-    console.log($(this).attr("data-name"));
-    $.ajax({
-      url: url,
-      method: "GET",
-    }).then(function () {});
-  });
+  $(document).on("click", ".card", function (event) {
+    const thisMovie = $(this).attr("data-name");
+    const singleMovieUrl =
+      "http://www.omdbapi.com/?&apikey=" + apiKey + "&t=" + thisMovie;
 
-  // $("#search-btn").on("click", function () {
-  //   $.ajax({
-  //     url: plotUrl + searchVal,
-  //     Method: "GET",
-  //   }).then(function (response) {
-  //     console.log("plot", response);
-  //   });
-  // });
+    $.ajax({
+      url: singleMovieUrl,
+      method: "GET",
+    }).then(function (data) {
+      console.log("data", data);
+      console.log(event.target);
+      const element = event.target;
+      if (element.matches("button")) {
+        window.location.href = "./favorites.html";
+      }
+    });
+  });
 });
