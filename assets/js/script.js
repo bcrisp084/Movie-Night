798a40ad6ca5250d232a37eabd6369ae7bb389bc
Brian Crisp <bcrisp084@yahoo.com>
2022-12-18 14:46:17 -0500

    Updated the styles and js to reflect adding a rotate effect to each movie card

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,19 @@ $(document).ready(function () {
     "http://www.omdbapi.com/?&apikey=" + apiKey + "&page=1&type=movie&s=";
 
   function reset() {
-    searchVal = "";
-    $(".results").empty();
+    $("#search-box").val("");
   }
 
   function displayUser() {
     var user = localStorage.getItem("user");
     $(".user").text(user);
+
+    if (user === null) {
+      window.location.href = "index.html";
+    }
   }
+
+  function cardBack() {}
 
   displayUser();
 
@@ -22,22 +27,22 @@ $(document).ready(function () {
       const element = response.Search[i];
       var image = $("<img>");
       image.attr("src", element.Poster);
+      image.attr("alt", "No Image Available");
       image.attr("class", "poster");
-      var cardDiv = $("<div>").attr("class", "card");
-      cardDiv.append(image);
+      var cardDiv = $("<div>").attr("class", "flip-card");
+      var cardInner = $("<div>").attr("class", "flip-card-inner");
+      var cardFront = $("<div>").attr("class", "flip-card-front");
+      var cardBack = $("<div>").attr("class", "flip-card-back");
+      var cardTitle = $("<h1>").text(element.Title);
+      var cardYear = $("<h3>").text(element.Year);
+      var cardType = $("<h3>").text(element.Type);
+      cardBack.append(cardTitle, cardYear, cardType);
+      cardInner.append(cardFront, cardBack);
+      cardFront.append(image);
+      cardDiv.append(cardInner);
       $(`#${section}`).append(cardDiv);
     }
   }
-
-  $("#go").on("click", function (event) {
-    reset();
-    event.preventDefault();
-    searchVal = $("#search-box").val().trim();
-    $.ajax({
-      url: url + searchVal,
-      method: "GET",
-    }).then(function (response) {});
-  });
 
   function displayComedy() {
     var comedyQuery =
@@ -66,4 +71,19 @@ $(document).ready(function () {
   }
   action();
   displayComedy();
+
+  $("#go").on("click", function (event) {
+    event.preventDefault();
+    console.log("this", $(this));
+    searchVal = $("#search-box").val().trim();
+    console.log(searchVal);
+    $.ajax({
+      url: url + searchVal,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      createCard(response, "searched");
+      reset();
+    });
+  });
 });
